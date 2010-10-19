bf1e733f8ea4dcaf0c879522a43534a4ef55c714
qurl <qurl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-19 18:03:00 +0000

    Updated readme.txt, bumping version number.
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-widgets/trunk@301808 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dynamic-widgets.php
+++ dynamic-widgets.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://www.qurl.nl/dynamic-widgets/
  * Description: Dynamic Widgets gives you more control over your widgets. It lets you dynamicly place widgets on pages by excluding or including rules by roles, dates, for the homepage, single posts, pages, authors, categories, archives, error page, search page and custom post types.
  * Author: Jacco
- * Version: 1.3.5
+ * Version: 1.3.6
  * Author URI: http://www.qurl.nl/
  * Tags: widget, widgets, dynamic, sidebar, custom, rules, admin, conditional tags
  *
@@ -42,7 +42,7 @@
   define('DW_PLUGIN', dirname(__FILE__) . '/' . 'plugin/');
   define('DW_TIME_LIMIT', 86400);				// 1 day
   define('DW_URL', 'http://www.qurl.nl');
-  define('DW_VERSION', '1.3.5');
+  define('DW_VERSION', '1.3.6');
   define('DW_VERSION_URL_CHECK', DW_URL . '/wp-content/uploads/php/dw_version.php?v=' . DW_VERSION . '&n=');
 	define('DW_WPML_API', '/inc/wpml-api.php');			// WPML Plugin support - API file relative to ICL_PLUGIN_PATH
 	define('DW_WPML_ICON', 'img/wpml_icon.png');	// WPML Plugin support - WPML icon
