cf6f474154956ac6a4df29e1871669215f15ccee
qurl <qurl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-23 19:15:28 +0000

    Updated readme.txt, bumping version.
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-widgets/trunk@349959 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dynamic-widgets.php
+++ dynamic-widgets.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://www.qurl.nl/dynamic-widgets/
  * Description: Dynamic Widgets gives you full control on which pages your widgets will appear. It lets you dynamicly place the widgets on WordPress pages.
  * Author: Qurl
- * Version: 1.3.7.10
+ * Version: 1.4.0
  * Author URI: http://www.qurl.nl/
  * Tags: widget, widgets, dynamic, sidebar, custom, rules, admin, conditional tags, wpml, wpec, buddypress
  *
@@ -58,7 +58,7 @@
   define('DW_PLUGIN', dirname(__FILE__) . '/' . 'plugin/');
   define('DW_TIME_LIMIT', 86400);				// 1 day
   define('DW_URL', 'http://www.qurl.nl');
-  define('DW_VERSION', '1.3.7.10');
+  define('DW_VERSION', '1.4.0');
   define('DW_VERSION_URL_CHECK', DW_URL . '/wp-content/uploads/php/dw_version.php?v=' . DW_VERSION . '&n=');
 	define('DW_WPML_API', '/inc/wpml-api.php');			// WPML Plugin support - API file relative to ICL_PLUGIN_PATH
 	define('DW_WPML_ICON', 'img/wpml_icon.png');	// WPML Plugin support - WPML icon
@@ -364,7 +364,7 @@
 
 	/**
 	 * dynwid_admin_wpec_dump() Dump WPEC rules function for upgrade to 3.8
-	 * @since 1.3.7.8
+	 * @since 1.4.0
 	 */
   function dynwid_admin_wpec_dump() {
   	$DW = &$GLOBALS['DW'];
